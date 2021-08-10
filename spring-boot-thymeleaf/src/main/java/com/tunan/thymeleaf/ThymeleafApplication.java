3b6816852fc0a1de0a40d04ee834e89a1272b3f6
yerias <1748094638@qq.com>
2021-08-10 22:55:23 +0800

    20210810

---
+++ spring-boot-thymeleaf/src/main/java/com/tunan/thymeleaf/ThymeleafApplication.java
@@ -0,0 +1,17 @@
+package com.tunan.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+/**
+ * @author Tunan
+ */
+@SpringBootApplication
+public class ThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafApplication.class, args);
+    }
+
+}
